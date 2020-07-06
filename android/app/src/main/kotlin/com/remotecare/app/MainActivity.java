ed1f6e3a8ee692f2abc98dbf16174b9872731dc1
gautam <gautam.3@iitj.ac.in>
2020-07-07 01:11:29 +0530

    changed device screen ui and seperated pulse input

--- android/app/src/main/kotlin/com/remotecare/app/MainActivity.java
+++ android/app/src/main/kotlin/com/remotecare/app/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends FlutterActivity {
     private static final String CHANNEL = "flutter.native/helper";
 
     public static  final int REQUEST_CODE_SPO2 = 0x011;
+    public static  final int REQUEST_CODE_PULSE = 0x010;
     private static final String TAG = MainActivity.class.getSimpleName();
     private MethodChannel.Result finalResult;
 
@@ -31,6 +32,11 @@ public class MainActivity extends FlutterActivity {
                         finalResult = result;
                         startActivityForResult(i,REQUEST_CODE_SPO2);
                     }
+                    else if(call.method.equals("fromPulse")){
+                        Intent i = new Intent(MainActivity.this, SPo2CaptureActivity.class);
+                        finalResult = result;
+                        startActivityForResult(i,REQUEST_CODE_PULSE);
+                    }
                 }
         );
     }
@@ -41,30 +47,21 @@ public class MainActivity extends FlutterActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-//        OxiMeterData spo2 = new  OxiMeterData();
-//        spo2.setPulse(120);
-//        spo2.setSpo2(85);
-//        spo2.setManual(false);
-//        Gson spo2Json = new Gson();
-//        String s = spo2Json.toJson(spo2);
-//        Log.d(TAG,spo2.toString());
-//        finalResult.success(s);
         if(data!=null){
             Bundle extras = data.getExtras();
             switch (requestCode){
                 case REQUEST_CODE_SPO2:
-
-//                    OxiMeterData spo2 = new  OxiMeterData();
-//                    spo2.setPulse(120);
-//                    spo2.setSpo2(85);
-//                    spo2.setManual(false);
-//                    Gson spo2Json = new Gson();
-//                    String s1 = spo2Json.toJson(spo2);
-//                    Log.d(TAG,spo2.toString());
-//                    finalResult.success(s1);
                     if(extras!=null){
                         OxiMeterData spo2 = (OxiMeterData) extras.getSerializable("spo2");
-//                        Log.d(TAG,spo2.toString());
+                        Gson spo2Json = new Gson();
+                        String s = spo2Json.toJson(spo2);
+                        Log.d(TAG,spo2.toString());
+                        finalResult.success(s);
+                    }
+                    break;
+                case REQUEST_CODE_PULSE:
+                    if(extras!=null){
+                        OxiMeterData spo2 = (OxiMeterData) extras.getSerializable("spo2");
                         Gson spo2Json = new Gson();
                         String s = spo2Json.toJson(spo2);
                         Log.d(TAG,spo2.toString());
